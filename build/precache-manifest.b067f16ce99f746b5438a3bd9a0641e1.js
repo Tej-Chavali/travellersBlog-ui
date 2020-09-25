3d097fe3ee644651f8e5e5e85a56ced502e91e7d
veena tej <tejaswichavali9@gmail.com>
2020-09-25 19:32:34 +0530

    fixed build issues

---
+++ build/precache-manifest.b067f16ce99f746b5438a3bd9a0641e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd4139edb0f884f9db17a657f36385f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427a1a2e56197e0baa86",
+    "url": "/static/css/2.b6a387d8.chunk.css"
+  },
+  {
+    "revision": "0e490e02e3ee97c73be6",
+    "url": "/static/css/main.a4c86466.chunk.css"
+  },
+  {
+    "revision": "427a1a2e56197e0baa86",
+    "url": "/static/js/2.f9e6da80.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.f9e6da80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e490e02e3ee97c73be6",
+    "url": "/static/js/main.5eafe876.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e0857ec19570eb2a660ff52132c185c.js"
+  "/precache-manifest.b067f16ce99f746b5438a3bd9a0641e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
